825c4962e7baa0ac51fc9a6c55e27f2993ea199d
Laxman Adhikari <ladhikari023@unm.edu>
2021-11-16 16:11:58 -0700

    PrimsGenerator class committed

---
+++ src/generators/PrimsGenerator.java
@@ -0,0 +1,12 @@
+package generators;
+
+import maze.Maze;
+
+public class PrimsGenerator extends MazeGenerator {
+
+    @Override
+    public Maze makeMaze(int size) {
+
+        return null;
+    }
+}
